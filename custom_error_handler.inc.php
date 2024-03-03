f87902d16742d5b6e9daf0ee731fb6579e9f3c64
Alfrey Chan <alfreychan@gmail.com>
2024-03-02 21:15:19 -0800

    Create custom error handling function for logging errors during production, and .gitignore

---
+++ custom_error_handler.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+function custom_error_handler($errno, $errstr, $errfile, $errline) {
+    $message = "Error: [$errno] $errstr\n$errfile: $errline";
+    error_log($message . PHP_EOL, 3, "error_log.txt");
+}
+
+set_error_handler("custom_error_handler");

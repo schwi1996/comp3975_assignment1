844af05d4566661b61c3a0ba197da97d971a4012
Alfrey Chan <alfreychan@gmail.com>
2024-03-02 23:04:46 -0800

    Change relative path to absolute path for error_log.txt

--- custom_error_handler.inc.php
+++ custom_error_handler.inc.php
@@ -2,7 +2,7 @@
 
 function custom_error_handler($errno, $errstr, $errfile, $errline) {
     $message = "Error: [$errno] $errstr\n$errfile: $errline";
-    error_log($message . PHP_EOL, 3, "error_log.txt");
+    error_log($message . PHP_EOL, 3, __DIR__ . "/error_log.txt");
 }
 
 set_error_handler("custom_error_handler");

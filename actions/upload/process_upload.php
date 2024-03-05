f46cb5e0f97d9c76dd3f307531e6f0e55d754062
Alfrey Chan <alfreychan@gmail.com>
2024-03-04 18:22:56 -0800

    Display error message if non csv file is uploaded

--- actions/upload/process_upload.php
+++ actions/upload/process_upload.php
@@ -5,38 +5,56 @@ require_once('../../custom_error_handler.inc.php');
 require_once('../../connect_database.php');
 // include("../buckets/process_buckets.php");
 
+function check_file_type($file) {
+    $file_type = pathinfo($file['name'], PATHINFO_EXTENSION);
+    if ($file_type !== 'csv') {
+        $_SESSION['error'] = "Only CSV files can be uploaded.";
+        header('Location: /actions/upload/upload.php');
+        die();
+    }
+}
+
+function create_imports_dir($dir) {
+    if (!is_dir($dir)) {
+        if (!mkdir($dir, 0777, true)) {
+            $_SESSION['error'] = "Error: Unable to create imports directory.";
+            header('Location: /actions/upload/upload.php');
+            die();
+        }
+    }
+}
+
+function process_CSV_file($file, $db, $dir) {
+    if (($handle = fopen($file['tmp_name'], 'r')) !== false) {
+        while (($row = fgetcsv($handle)) !== false) {
+            $stmt = $db -> prepare('INSERT INTO Transactions (Date, Vendor, Spend, Deposit, Balance) VALUES (:date, :vendor, :spend, :deposit, :balance)');
+            $stmt -> bindValue(':date', $row[0]);
+            $stmt -> bindValue(':vendor', $row[1]);
+            $stmt -> bindValue(':spend', $row[2]);
+            $stmt -> bindValue(':deposit', $row[3]);
+            $stmt -> bindValue(':balance', $row[4]);
+
+            $stmt -> execute();
+        }
+        fclose($handle);
+
+        $file_name = pathinfo($file['name'], PATHINFO_FILENAME);
+        $path = $dir .  '/' . $file_name . '.imported';
+        if (!move_uploaded_file($file['tmp_name'], $path)) {
+            echo "Error: Unable to move file.";
+        }
+    }
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_FILES['csvFile']) && $_FILES['csvFile']['error'] == 0) {
-        $csv_file = $_FILES['csvFile']['tmp_name'];
-        
+        check_file_type($_FILES['csvFile']);
+
         $imports_dir = __DIR__ . '/../../imports';
-        if (!is_dir($imports_dir)) {
-            if (!mkdir($imports_dir, 0777, true)) {
-                $_SESSION['error'] = "Error: Unable to create imports directory.";
-                header('Location: /actions/upload/upload.php');
-                die();
-            }
-        }
+        create_imports_dir($imports_dir);
 
-        if (($handle = fopen($csv_file, 'r')) !== false) {
-            while (($row = fgetcsv($handle)) !== false) {
-                $stmt = $db -> prepare('INSERT INTO Transactions (Date, Vendor, Spend, Deposit, Balance) VALUES (:date, :vendor, :spend, :deposit, :balance)');
-                $stmt -> bindValue(':date', $row[0]);
-                $stmt -> bindValue(':vendor', $row[1]);
-                $stmt -> bindValue(':spend', $row[2]);
-                $stmt -> bindValue(':deposit', $row[3]);
-                $stmt -> bindValue(':balance', $row[4]);
+        process_CSV_file($_FILES['csvFile'], $db, $imports_dir);
 
-                $stmt -> execute();
-            }
-            fclose($handle);
-
-            $file_name = pathinfo($_FILES['csvFile']['name'], PATHINFO_FILENAME);
-            $path = $imports_dir .  '/' . $file_name . '.imported';
-            if (!move_uploaded_file($csv_file, $path)) {
-                echo "Error: Unable to move file.";
-            }
-        }
         $db -> close();
         header('Location: /actions/landing/landing.php');
         die();
